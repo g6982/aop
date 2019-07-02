90d40f2fd0a2ad897d9e3fe3d0fc256e4005fca3
1di0t <dgqcjx@gmail.com>
2019-07-02 15:39:53 +0800

    自动获取线路和服务产品，以及生成税务发票

--- aop_sale/models/account_tax_invoice.py
+++ aop_sale/models/account_tax_invoice.py
@@ -18,8 +18,12 @@ class AccountTaxInvoice(models.Model):
     _name = 'account.tax.invoice'
     _description = 'account tax invoice'
 
+    _sql_constrain = [('unique_tax_no', 'unique(tax_invoice_no, company_id)', 'Tax invoice no must be unique!')]
+
     invoice_line_ids = fields.One2many('account.tax.invoice.line', 'invoice_id', string='Invoice Lines',
                                        readonly=True, states={'draft': [('readonly', False)]}, copy=True)
+
+    tax_invoice_no = fields.Char('Tax invoice no')
 
 
 class AccountTaxInvoiceLine(models.Model):
--- aop_sale/wizard/account_tax_invoice_wizard.py
+++ aop_sale/wizard/account_tax_invoice_wizard.py
@@ -10,13 +10,14 @@ class AccountTaxInvoiceWizard(models.TransientModel):
     _name = 'account.tax.invoice.wizard'
     _description = 'the way to make tax invoice'
 
-    tax_invoice_no = fields.Char('Tax invoice no')
+    tax_invoice_no = fields.Char('Tax invoice no', required=True)
+    partner_id = fields.Many2one('res.partner', 'Partner')
     invoice_line_ids = fields.Many2many('account.invoice.line', string='Invoice line')
     tax_invoice_method = fields.Selection([
         ('all', 'All'),
         ('part_amount', 'Part-amount'),
         ('part_percent', 'Part-rate'),
-    ])
+    ], required=True)
     tax_invoice_number = fields.Float('Tax invoice number')
 
     @api.model
@@ -25,12 +26,77 @@ class AccountTaxInvoiceWizard(models.TransientModel):
         if self.env.context.get('active_ids'):
             invoice_ids = self.env['account.invoice'].browse(self.env.context.get('active_ids'))
             invoice_line_ids = invoice_ids.mapped('invoice_line_ids')
+            partner_id = invoice_line_ids.mapped('partner_id')
             invoice_line_ids = invoice_line_ids.filtered(lambda x: x.tax_invoice_amount != x.price_subtotal)
             res.update({
-                'invoice_line_ids': [(6, 0, invoice_line_ids.ids)]
+                'invoice_line_ids': [(6, 0, invoice_line_ids.ids)],
+                'partner_id': partner_id[0].id
             })
         return res
 
-    def create_account_tax_invoice(self):
-        pass
+    # 解析数据
+    def _parse_account_tax_invoice_data(self):
+        data = {
+            'partner_id': self.partner_id.id,
+            'partner_shipping_id': self.partner_id.id,
+            'user_id': self.env.user.id,
+            'tax_invoice_no': self.tax_invoice_no
+        }
+        return data
 
+    # 数据行的数据
+    def _parse_account_tax_invoice_line_data(self, tax_invoice_method, tax_invoice_number, invoice_line_ids):
+        line_data = []
+        for line_id in invoice_line_ids:
+            # 获取金额数据
+            amount = getattr(self, '_compute_invoice_amount_{type_name}'.format(
+                type_name=tax_invoice_method
+            ))(line_id, tax_invoice_number, invoice_line_ids)
+            line_data.append((0, 0, {
+                'product_id': line_id.product_id.id,
+                'name': line_id.product_id.name,
+                'quantity': 1,
+                'uom_id': line_id.product_id.uom_id.id,
+                'price_unit': amount,
+                'account_id': line_id.account_id.id,
+            }))
+        return line_data
+
+    def _compute_invoice_amount_all(self, line_id, tax_invoice_number, invoice_line_ids):
+        left_amount = line_id.price_subtotal - line_id.tax_invoice_amount
+        line_id.write({
+            'tax_invoice_amount': line_id.price_subtotal
+        })
+        return round(float(left_amount) * 1000, -1) / 1000
+
+    def _compute_invoice_amount_part_amount(self, line_id, tax_invoice_number, invoice_line_ids):
+        left_amount = line_id.price_subtotal - line_id.tax_invoice_amount
+        if left_amount > tax_invoice_number / len(invoice_line_ids):
+            line_id.write({
+                'tax_invoice_amount': line_id.tax_invoice_amount + round(
+                    float(tax_invoice_number / len(invoice_line_ids) * 1000), -1) / 1000
+            })
+            return round(float(tax_invoice_number / len(invoice_line_ids) * 1000), -1) / 1000
+        else:
+            line_id.write({
+                'tax_invoice_amount': line_id.price_subtotal
+            })
+            return left_amount
+
+    def _compute_invoice_amount_part_percent(self, line_id, tax_invoice_number, invoice_line_ids):
+        res = (line_id.price_subtotal - line_id.tax_invoice_amount) * (tax_invoice_number / 100)
+        line_id.write({
+            'tax_invoice_amount': line_id.tax_invoice_amount + round(float(res * 1000), -1) / 1000
+        })
+        return round(float(res * 1000), -1) / 1000
+
+    def create_account_tax_invoice(self):
+        data = self._parse_account_tax_invoice_data()
+        line_data = self._parse_account_tax_invoice_line_data(self.tax_invoice_method, self.tax_invoice_number,
+                                                              self.invoice_line_ids)
+
+        data.update({
+            'invoice_line_ids': line_data
+        })
+        tax_invoice = self.env['account.tax.invoice']
+        res = tax_invoice.create(data)
