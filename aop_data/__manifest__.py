af760d9c7da04a3fd38f10e360cdab19dc80976d
waynexiao567890 <13671898587@163.com>
2019-07-15 15:05:22 +0800

    重新整理数据,合同条款翻译

--- aop_data/__manifest__.py
+++ aop_data/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'sale', 'sale_stock', 'account', 'purchase', 'hr_expense', 'product'],
+    'depends': ['base', 'stock', 'sale', 'sale_stock', 'account', 'purchase', 'hr_expense', 'product', 'aop_sale'],
 
     # always loaded
     'data': [
