0d187ec6041ae20854a9980dac49571c99fafd4f
1di0t <dgqcjx@gmail.com>
2019-07-27 15:35:28 +0800

    去掉颜色的查找

--- aop_sale/models/stock_picking_type.py
+++ aop_sale/models/stock_picking_type.py
@@ -7,3 +7,9 @@ class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
     service_product_id = fields.Many2one('product.product', 'Service product')
+
+    code = fields.Selection([
+        ('incoming', 'Vendors'),
+        ('outgoing', 'Customers'),
+        ('internal', 'Internal')
+    ], 'Type of Operation', required=True, default='internal')
