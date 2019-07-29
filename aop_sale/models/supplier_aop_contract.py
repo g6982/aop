5aca2aaeebc8e87848123beb76753e576020d6a4
1di0t <dgqcjx@gmail.com>
2019-07-29 09:51:54 +0800

    修改查找规则的方法，使用warehouse筛选location，再根据location递归筛选所有child_location

--- aop_sale/models/supplier_aop_contract.py
+++ aop_sale/models/supplier_aop_contract.py
@@ -21,8 +21,39 @@ class SupplierAopContract(models.Model):
         for line in self:
             if line.partner_id:
                 allow_warehouse_ids = line.partner_id.allow_warehouse_ids.ids
-                rules = self.env['stock.rule'].search([('warehouse_id', 'in', allow_warehouse_ids)])
+                rules = self.find_all_rule_by_location(allow_warehouse_ids)
                 line.rule_ids = [(6, 0, rules.ids)]
+
+    # 通过位置查找规则
+    def find_all_rule_by_location(self, allow_warehouse_ids):
+        '''
+        :param allow_warehouse_ids: 允许的仓库
+        :return: 允许的规则
+        '''
+        warehouse_ids = self.env['stock.warehouse'].browse(allow_warehouse_ids)
+        location_ids = warehouse_ids.mapped('lot_stock_id')
+        res = []
+        for location_id in location_ids:
+            self.find_all_location(location_id, res)
+        rules = self.env['stock.rule'].search([
+            '|',
+            ('location_src_id', 'in', res),
+            ('location_id', 'in', res)
+        ])
+        return rules
+
+    # 递归查找
+    def find_all_location(self, parent_location_id, res):
+        '''
+        :param parent_location_id: 位置
+        :param res: 保存位置的 []
+        :return: 所有位置的 []
+        '''
+        res.append(parent_location_id.id)
+        if not parent_location_id.child_ids:
+            return res
+        for child_id in parent_location_id.child_ids:
+            self.find_all_location(child_id, res)
 
 
 class SupplierStockRuleLine(models.Model):
