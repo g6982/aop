61d5bf6e9a1a5efdf064817eac5a44cd836f61a5
1di0t <dgqcjx@gmail.com>
2019-08-07 08:46:50 +0800

    fix bug

--- aop_sale/models/delivery_carrier.py
+++ aop_sale/models/delivery_carrier.py
@@ -38,8 +38,8 @@ class DeliveryCarrier(models.Model):
     from_location_id = fields.Many2one('stock.location', 'From location')
     to_location_id = fields.Many2one('stock.location', 'To location')
 
-    from_location_ids = fields.Many2one('stock.location', 'From locations')
-    to_location_ids = fields.Many2one('stock.location', 'To locations')
+    from_location_ids = fields.Many2many('stock.location', string='From locations')
+    to_location_ids = fields.Many2many('stock.location', string='To locations')
 
     rule_ids = fields.Many2many('stock.rule', string='Rules', related='supplier_contract_id.rule_ids', readonly=True)
     rule_id = fields.Many2one('stock.rule', string='Rule')
