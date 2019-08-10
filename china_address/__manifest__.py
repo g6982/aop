47c4832f5b61bf8ad17092051057d48ae8a11ee8
waynexiao567890 <13671898587@163.com>
2019-08-10 20:54:45 +0800

    新增省市县3级联动模块"china_address"

---
+++ china_address/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018 ABC_CODER designer.shaoyan@gmail.com
+
+{
+    'name': '中国省市县',
+    'version': '0.1',
+    'category': 'Base',
+    'author': 'ABC_CODER',
+    'maintainer': 'designer.shaoyan@gmail.com',
+    'description': """
+
+    添加中文市县数据
+
+    添加partner三级联动
+
+    """,
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/res_country_state_data.xml',
+        'data/res_city_data.xml',
+        'data/res_district_data.xml',
+        'views/china_address_views.xml'
+    ]
+}
