5a2783c51b9e42340aee83c97605e5e6f7380c32
waynexiao567890 <13671898587@163.com>
2019-09-26 14:10:38 +0800

    fix

--- aop_sale/models/account_invoice.py
+++ aop_sale/models/account_invoice.py
@@ -314,7 +314,7 @@ class AccountInvoiceLine(models.Model):
     @api.depends('sale_order_line_id', 'sale_order_line_id.state')
     def _compute_advance_receipt(self):
         for line in self:
-            if line.account_period_id.monthly_state:
+            if line.invoice_id.account_period_id.monthly_state:
                 continue
             if line.sale_order_line_id.picking_confirm_date:
                 line.advance_receipt = 0
