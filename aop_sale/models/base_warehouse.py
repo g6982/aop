60e1ef4b480bb9552a969ef3781cc5e7e5597f5d
1di0t <dgqcjx@gmail.com>
2019-10-12 15:05:28 +0800

    基地添加上下级

--- aop_sale/models/base_warehouse.py
+++ aop_sale/models/base_warehouse.py
@@ -10,6 +10,22 @@ class BaseWarehouse(models.Model):
     _name = 'base.warehouse'
     _description = 'Base warehouse'
     _sql_constraints = [('unique_name', 'unique(name)', 'must unique!')]
+    _parent_name = "parent_id"
+    _parent_store = True
 
     name = fields.Char('Name')
     warehouse_ids = fields.Many2many('stock.warehouse')
+
+    parent_id = fields.Many2one(
+        'base.warehouse', 'Parent base warehouse', index=True, ondelete='cascade')
+    child_ids = fields.One2many('base.warehouse', 'parent_id', 'Contains')
+    parent_path = fields.Char(index=True)
+
+    @api.multi
+    @api.constrains('parent_id')
+    def check_recursion(self):
+        for base_id in self:
+            if not super(BaseWarehouse, base_id)._check_recursion():
+                raise UserError(
+                    _('You can not create recursive base warehouse.'),
+                )
