ef7fd2218274100a9ba5bd2002300d26841e6d72
1di0t <dgqcjx@gmail.com>
2019-10-12 14:25:18 +0800

    stock.picking.type clear cache

--- aop_sale/models/stock_picking_type.py
+++ aop_sale/models/stock_picking_type.py
@@ -13,3 +13,8 @@ class StockPickingType(models.Model):
         ('outgoing', 'Customers'),
         ('internal', 'Internal')
     ], 'Type of Operation', required=True, default='internal')
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
+        self.env['ir.rule'].clear_cache()
+        return super(StockPickingType, self).search_read(domain, fields, offset, limit, order)
