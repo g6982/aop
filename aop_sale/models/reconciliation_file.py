aa2e15264e7044132d27ab4fe0949a5e9c1f4fe4
1di0t <dgqcjx@gmail.com>
2019-10-18 15:53:31 +0800

    对帐

--- aop_sale/models/reconciliation_file.py
+++ aop_sale/models/reconciliation_file.py
@@ -19,35 +19,68 @@ class ReconciliationFile(models.Model):
     price_total = fields.Float('Price total')
     product_id = fields.Many2one('product.product', 'Product')
 
-    invoice_line_ids = fields.Many2many('account.invoice.line', string='Invoice line')
+    re_line_ids = fields.One2many('reconciliation.file.line', 're_file_id')
+
+    # @api.multi
+    # def reconciliation_account_invoice(self):
+    #     for line in self:
+    #         invoice_line_id = self.env['account.invoice.line'].search([
+    #             ('sale_order_line_id.handover_number', '=', line.name),
+    #             ('sale_order_line_id.product_id', '=', line.product_id.id),
+    #             ('invoice_id.state', '=', 'draft')
+    #         ])
+    #
+    #         if not invoice_line_id:
+    #             continue
+    #
+    #         # FIXME: 筛选数量？
+    #         selected_ids = invoice_line_id.mapped('invoice_id').ids[:line.number]
+    #         invoice_line_id = invoice_line_id.filtered(lambda x: x.invoice_id.id in selected_ids)
+    #
+    #         line.invoice_line_ids = [(6, 0, invoice_line_id.ids)]
+    #
+    #         # 对账：需要将单车价格写入确认价格
+    #         for account_invoice_line_id in invoice_line_id:
+    #             account_invoice_line_id.price_unit = line.price_unit
+    #
+    #         # TODO: 如果 单车价格和合同价格相同，则对账 ?
+    #         # invoice_line_id = invoice_line_id.filtered(lambda x: x.price_unit == x.contract_price)
+    #
+    #         for invoice_id in invoice_line_id.mapped('invoice_id'):
+    #             invoice_id.action_invoice_open()
 
     @api.multi
     def reconciliation_account_invoice(self):
         for line in self:
-            invoice_line_id = self.env['account.invoice.line'].search([
-                ('sale_order_line_id.handover_number', '=', line.name),
-                ('sale_order_line_id.product_id', '=', line.product_id.id),
-                ('invoice_id.state', '=', 'draft')
-            ])
+            sale_order_line_ids = self.env['sale.order.line'].search([
+                ('handover_number', '=', line.name),
+                ('product_id', '=', line.product_id.id),
+            ], limit=line.number)
 
-            if not invoice_line_id:
+            if not sale_order_line_ids:
                 continue
 
-            # FIXME: 筛选数量？
-            selected_ids = invoice_line_id.mapped('invoice_id').ids[:line.number]
-            invoice_line_id = invoice_line_id.filtered(lambda x: x.invoice_id.id in selected_ids)
+            not_invoice_line = sale_order_line_ids.filtered(lambda x: not x.invoice_lines)
+            data = []
+            for invoice_id in sale_order_line_ids.mapped('invoice_lines'):
+                data.append((0, 0, {
+                    'sale_order_line_id': invoice_id.sale_order_line_id.id,
+                    'invoice_line_id': invoice_id
+                }))
+            data.append((0, 0, {
+                'invoice_line_id': x.id,
+                'sale_order_line_id': False
+            }) for x in not_invoice_line)
 
-            line.invoice_line_ids = [(6, 0, invoice_line_id.ids)]
+            line.re_line_ids = data
 
-            # 对账：需要将单车价格写入确认价格
-            for account_invoice_line_id in invoice_line_id:
-                account_invoice_line_id.price_unit = line.price_unit
 
-            # TODO: 如果 单车价格和合同价格相同，则对账 ?
-            # invoice_line_id = invoice_line_id.filtered(lambda x: x.price_unit == x.contract_price)
+class ReconciliationFileLine(models.Model):
+    _name = 'reconciliation.file.line'
 
-            for invoice_id in invoice_line_id.mapped('invoice_id'):
-                invoice_id.action_invoice_open()
+    re_file_id = fields.Many2one('reconciliation.file')
+    invoice_line_id = fields.Many2one('account.invoice.line', string='Invoice line')
+    sale_order_line_id = fields.Many2one('sale.order.line', string='Order line')
 
 
 class BaseImport(models.TransientModel):
