fd8f70e4fd00e27075fd842f56cf96781c6dadae
1di0t <dgqcjx@gmail.com>
2019-11-27 21:27:16 +0800

    merge

--- test_aop_interface.py
+++ test_aop_interface.py
@@ -7,8 +7,8 @@ import string
 import requests
 import json
 
-BASE_URL = 'http://47.103.54.14:8069'
-# BASE_URL = 'http://127.0.0.1:8069'
+# BASE_URL = 'http://47.103.54.14:8069'
+BASE_URL = 'http://127.0.0.1:8069'
 
 TOKEN_URL = '/api/auth/token'
 DONE_PICKING = '/api/stock_picking/done_picking'
@@ -25,13 +25,13 @@ data = {
     }
 }
 
-# data = {
-#     'params': {
-#         'login': 'odoo',
-#         'password': 'odoo',
-#         'db': 'aopdata'
-#     }
-# }
+data = {
+    'params': {
+        'login': 'odoo',
+        'password': 'odoo',
+        'db': 'aop27'
+    }
+}
 # 测试 获取 token
 req = requests.post(url=BASE_URL + TOKEN_URL, data=json.dumps(data), headers=HEADERS)
 content = json.loads(req.content.decode('utf-8'))
@@ -43,51 +43,65 @@ HEADERS.update({
     'access-token': result.get('access_token')
 })
 print('headers: ', HEADERS)
+
+
 # 返回值： headers:  {'Content-Type': 'application/json', 'charset': 'utf-8', 'access-token': 'access_token_f772d5c69ca510ad54eb91c8d8072e9dd4a2538d'}
 
 # salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
 
 
-def get_17_vin():
-    return ''.join(random.sample(string.ascii_letters + string.digits, 17))
+# def get_17_vin():
+#     return ''.join(random.sample(string.ascii_letters + string.digits, 17))
+#
+#
+# def send_data_test(loop_time):
+#     warehouse_code = 'WTJCXBK'
+#     warehouse_name = '团结村线边库'
+#     for _ in range(loop_time):
+#         data = {
+#             'params': {
+#                 'data': [{'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
+#                           'vin': get_17_vin(), 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name},
+#                          ]
+#             }
+#         }
+#         res = requests.post(url=BASE_URL + DONE_PICKING, data=json.dumps(data), headers=HEADERS)
+#         content = json.loads(res.content.decode('utf-8'))
+#     print(content)
+#
+#
+# if __name__ == '__main__':
+#     send_data_test(300)
 
 
-# SLRZ/雒容站
-# WTJZXBK/唐家沱线边库
-# WTJCXBK/团结村线边库
-# WXZTLK/新筑铁路库
-warehouse_code = 'WTJCXBK'
-warehouse_name = '团结村线边库'
-# 接车数据
-data = {
-    'params': {
-        'data': [{'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-                  'vin': 'WW000000000000003', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name},
-                 {'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-                  'vin': 'WW000000000000004', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name},
-                 {'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-                  'vin': 'WW000000000000005', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name}]
-    }
-}
-
+# # SLRZ/雒容站
+# # WTJZXBK/唐家沱线边库
+# # WTJCXBK/团结村线边库
+# # WXZTLK/新筑铁路库
+# warehouse_code = 'WTJCXBK'
+# warehouse_name = '团结村线边库'
+# # 接车数据
+# data = {
+#     'params': {
+#         'data': [{'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
+#                   'vin': 'WW000000000000113', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name},
+#                  {'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
+#                   'vin': 'WW000000000000115', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name}
+#                  ]
+#     }
+# }
+#
 # # 完成任务
 # # 227, 236, 254, 245
 # # 测试 I_WMS_AOP_001
 data = {
     'params': {
         'data': [
-            # {
-            #     'task_id': 11,
-            # },
-            # {
-            #     'task_id': 20,
-            # },
-            # {
-            #     'task_id': 29,
-            # },
             {
-                'task_id': 30
-            }
+                'task_id': 43,
+                'sequence_id': 3
+            },
+
         ]
     }
 }
@@ -95,11 +109,11 @@ data = {
 res = requests.post(url=BASE_URL + DONE_PICKING, data=json.dumps(data), headers=HEADERS)
 content = json.loads(res.content.decode('utf-8'))
 print(content)
-# 返回值： {'jsonrpc': '2.0', 'result': '{"time": 1571821697.9542348, "method": "/api/stock_picking/done_picking", "code": 200}', 'id': None}
+# # 返回值： {'jsonrpc': '2.0', 'result': '{"time": 1571821697.9542348, "method": "/api/stock_picking/done_picking", "code": 200}', 'id': None}
 
 
-# # 测试WMS
-# # 使用 zeep 发送数据
+# 测试WMS
+# 使用 zeep 发送数据
 # from requests import Session
 # from zeep.transports import Transport
 # from zeep import Client
@@ -110,57 +124,62 @@ print(content)
 #
 # # 客户供应商
 # zeep_supplier_client = Client('https://222.66.94.66:9081/zjwms/ws/supplierInfo?wsdl', transport=transport)
+# zeep_picking_client = Client('https://222.66.94.66:9081/zjwms/ws/generationTask?wsdl', transport=transport)
 #
-#
-# data = []
-# for _ in range(1000):
-#     tmp = {
-#         'name': '陕西福腾汽车贸易有限公司',
-#         'code': 'A02121',
-#         'country_name': 'China',
-#         'state_name': '陕西省 (CN)',
-#         'city_name': '西安市',
-#         'street_name': '陕西省西安市未央区三桥西部车城陕西福腾汽车贸易有限公司',
-#         'time': time.time()
-#     }
-#
-#     data.append(tmp)
-# # data = [
-# #     {
+# # data = []
+# # for _ in range(1000):
+# #     tmp = {
 # #         'name': '陕西福腾汽车贸易有限公司',
 # #         'code': 'A02121',
 # #         'country_name': 'China',
 # #         'state_name': '陕西省 (CN)',
 # #         'city_name': '西安市',
-# #         'street_name': '陕西省西安市未央区三桥西部车城陕西福腾汽车贸易有限公司'
-# #     },
-# #     {
-# #         'name': '西昌市成品装卸搬运服务有限公司',
-# #         'code': 'XC0533'
-# #     },
-# #     {
-# #         'name': '重庆市禾宏物流有限公司',
-# #         'code': 'A02323'
-# #     }]
-# res = zeep_supplier_client.service.supplier(str(data))
-# print(res)
-#
-#
-# # # 任务
-# # data = [{
-# #     'task_id': 589,
-# #     'partner_name': '长安民生',
-# #     'product_id': '福睿斯',
-# #     'from_location_id': '福特二厂VDC库1',
-# #     'to_location_id': '团结村线边库',
-# #     'picking_type_name': '接车',
-# #     'quantity_done': 1
-# # }]
-# # zeep_task_client = Client('https://222.66.94.66:9081/zjwms/ws/generationTask?wsdl', transport=transport)
-#
-# # res = zeep_task_client.service.sendToTask(str(data))
+# #         'street_name': '陕西省西安市未央区三桥西部车城陕西福腾汽车贸易有限公司',
+# #         'time': time.time()
+# #     }
+# #
+# #     data.append(tmp)
+# # # data = [
+# # #     {
+# # #         'name': '陕西福腾汽车贸易有限公司',
+# # #         'code': 'A02121',
+# # #         'country_name': 'China',
+# # #         'state_name': '陕西省 (CN)',
+# # #         'city_name': '西安市',
+# # #         'street_name': '陕西省西安市未央区三桥西部车城陕西福腾汽车贸易有限公司'
+# # #     },
+# # #     {
+# # #         'name': '西昌市成品装卸搬运服务有限公司',
+# # #         'code': 'XC0533'
+# # #     },
+# # #     {
+# # #         'name': '重庆市禾宏物流有限公司',
+# # #         'code': 'A02323'
+# # #     }]
+# # res = zeep_supplier_client.service.supplier(str(data))
 # # print(res)
 #
+#
+# # 任务
+# data = [{'task_id': 71, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'EM',
+#          'supplier_name': '重庆上源货运代理有限公司', 'warehouse_code': 'WTJCXBK', 'quantity_done': 1, 'brand_model_name': '长安福特',
+#          'from_location_id': '团结村线边库', 'to_location_id': '团结村站', 'to_location_type': 'internal', 'partner_name': '长安民生',
+#          'vin': 'LVSHFFAU3KS911441', 'picking_type_name': '装车', 'batch_id': 1,
+#          'scheduled_date': '2019-11-27 07:15:42'},
+#         {'task_id': 80, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'EM',
+#          'supplier_name': '重庆上源货运代理有限公司', 'warehouse_code': 'WTJCXBK', 'quantity_done': 1, 'brand_model_name': '长安福特',
+#          'from_location_id': '团结村线边库', 'to_location_id': '团结村站', 'to_location_type': 'internal', 'partner_name': '长安民生',
+#          'vin': 'LVSHFFAU7KS910681', 'picking_type_name': '装车', 'batch_id': 1,
+#          'scheduled_date': '2019-11-27 07:15:42'},
+#         {'task_id': 89, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'EM',
+#          'supplier_name': '重庆上源货运代理有限公司', 'warehouse_code': 'WTJCXBK', 'quantity_done': 1, 'brand_model_name': '长安福特',
+#          'from_location_id': '团结村线边库', 'to_location_id': '团结村站', 'to_location_type': 'internal', 'partner_name': '长安民生',
+#          'vin': 'LVSHFFAU0KS910893', 'picking_type_name': '装车', 'batch_id': 1,
+#          'scheduled_date': '2019-11-27 07:15:42'}]
+#
+# res = zeep_picking_client.service.sendToTask(str(data))
+# print(res)
+
 # [{
 #     'vin': 'LVSHFFAU6KS848090',
 #     'task_id': 2,
