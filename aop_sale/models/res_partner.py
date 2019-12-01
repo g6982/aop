027fa02484e3f9a3745d4f4eaf50a7e2ee9b6fd4
1di0t <dgqcjx@gmail.com>
2019-12-01 20:37:37 +0800

    remove

--- aop_sale/models/res_partner.py
+++ aop_sale/models/res_partner.py
@@ -83,15 +83,15 @@ class ResPartner(models.Model):
                 res.send_res_partner_to_wms()
         return res
 
-    # TODO: 对用户进行分类，客户，供应商，仓库的合作伙伴，用户的合作伙伴，位置的合作伙伴，公司的合作伙伴
-    user_usage_type = fields.Selection([
-        ('warehouse_type', 'Warehouse type'),
-        ('location_type', 'Location type'),
-        ('customer_type', 'Customer type'),
-        ('supplier_type', 'Supplier type'),
-        ('user_type', 'User type'),
-        ('company_type', 'Company type')
-    ], default=False)
+    # # TODO: 对用户进行分类，客户，供应商，仓库的合作伙伴，用户的合作伙伴，位置的合作伙伴，公司的合作伙伴
+    # user_usage_type = fields.Selection([
+    #     ('warehouse_type', 'Warehouse type'),
+    #     ('location_type', 'Location type'),
+    #     ('customer_type', 'Customer type'),
+    #     ('supplier_type', 'Supplier type'),
+    #     ('user_type', 'User type'),
+    #     ('company_type', 'Company type')
+    # ], default=False)
 
 
 class BaseImport(models.TransientModel):
