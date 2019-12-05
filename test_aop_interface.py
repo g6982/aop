2485f415f00f81bae3b03e58a691c15ab71893ed
1di0t <dgqcjx@gmail.com>
2019-12-05 22:08:12 +0800

    fix

--- test_aop_interface.py
+++ test_aop_interface.py
@@ -6,7 +6,6 @@ import random
 import string
 import requests
 import json
-import pprint
 
 # BASE_URL = 'http://47.103.54.14:8069'
 BASE_URL = 'http://127.0.0.1:8069'
@@ -23,17 +22,17 @@ data = {
     'params': {
         'login': 'aop_interface_test_name',
         'password': 'aop_interface_test_password',
-        'db': 'daodu_demo'
+        'db': 'aop3'
     }
 }
 
-data = {
-    'params': {
-        'login': 'odoo',
-        'password': 'odoo',
-        'db': 'aop27'
-    }
-}
+# data = {
+#     'params': {
+#         'login': 'odoo',
+#         'password': 'odoo',
+#         'db': 'aop27'
+#     }
+# }
 # 测试 获取 token
 req = requests.post(url=BASE_URL + TOKEN_URL, data=json.dumps(data), headers=HEADERS)
 content = json.loads(req.content.decode('utf-8'))
@@ -46,14 +45,33 @@ HEADERS.update({
 })
 print('headers: ', HEADERS)
 
+batch_id = self.env['stock.picking.batch'].browse(1)
+data = []
+for task_id in batch_id.picking_ids:
+    tmp = {
+        'task_id': task_id
+    }
+    data.append(tmp)
+# 测试 I_WMS_AOP_001
+data = {
+    'params': {
+        'data': data
+    }
+}
+
+res = requests.post(url=BASE_URL + DONE_PICKING, data=json.dumps(data), headers=HEADERS)
+content = json.loads(res.content.decode('utf-8'))
+print(content)
 
 # 返回值： headers:  {'Content-Type': 'application/json', 'charset': 'utf-8', 'access-token': 'access_token_f772d5c69ca510ad54eb91c8d8072e9dd4a2538d'}
 
 # salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
 
 
-# def get_17_vin():
-#     return ''.join(random.sample(string.ascii_letters + string.digits, 17))
+def get_17_vin():
+    return ''.join(random.sample(string.ascii_letters + string.digits, 17))
+
+
 #
 #
 # def send_data_test(loop_time):
@@ -96,94 +114,105 @@ print('headers: ', HEADERS)
 # # 完成任务
 # # 227, 236, 254, 245
 # # 测试 I_WMS_AOP_001
-data = {
-    'params': {
-        'data': [
-            {
-                'task_id': 43,
-                'sequence_id': 3
-            },
-
-        ]
-    }
-}
+# data = {
+#     'params': {
+#         'data': [
+#             {
+#                 'task_id': 43,
+#                 'sequence_id': 3
+#             },
+#
+#         ]
+#     }
+# }
 # SLRZ/雒容站
 # WTJZXBK/唐家沱线边库
 # WTJCXBK/团结村线边库
 # WXZTLK/新筑铁路库
-warehouse_code = 'WTJCXBK'
-warehouse_name = '团结村线边库'
+warehouse_code = 'WTJCKYK'
+warehouse_name = '团结村快运库'
+#
+# warehouse_code = 'WTJCXBK'
+# warehouse_name = '团结村线边库'
 # 接车数据
-# data = {
-#     'params': {
-#         'data': [{'brand_model_name': 'CQ2VDC1', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-#                   'vin': 'XX0001', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name},
-#                  # {'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-#                  #  'vin': 'WW000000000000004', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name},
-#                  # {'brand_model_name': 'CQ1VDC2', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-#                  #  'vin': 'WW000000000000005', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name}
-#                  ]
-#     }
-# }
+
+data1 = []
+index = 128
+for _ in range(6):
+    tmp = {'brand_model_name': 'WFTCZK', 'product_color': 'YG4F', 'product_model': '86H', 'state_flag': 'T',
+           'vin': 'WwidqDp5CYhQAC' + str(index), 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name}
+    data1.append(tmp)
+    index += 1
 
 data = {
     'params': {
-        'data': []
+        'data': data1
     }
 }
-
-data_lst = data['params']['data']
-
-source_date = [('LVSHJCAL5KE911041', '87K'),('LVSHFFAU2KS911432', '874'),('LVSHFFAU2KS911477', '874'),('LVSHFFAU3KS911438', '874'),
-                   ('LVSHFFAU5KS911702', '874'),('LVSHFFAU3KS911441', '874'),('LVSHFFAU7KS910681', '874'),('LVSHFFAU0KS910893', '874'),
-                   ('LVSHJCAL6KE908925', '87K'),('LVSHFFAUXKS911744', '874'),('LVSHFFAU8KS911435', '874'),('LVSHFFAU9KS911752', '874'),
-                   ('LVSHFFAUXKS911436', '874'),('LVSHFFAU5KS911750', '874'),('LVSHJCAL0KE910136', '87K'),('LVSHJCAL5KE910083', '87K'),
-                   ('LVSHFFAU1KS911745', '874'),('LVSHFFAUXKS910786', '874'),('LVSHJCAL8KE908926', '87K'),('LVSHFFAU7KS911457', '874'),
-                   ('LVSHFFAU3KS911455', '874'),('LVSHFFAU1KS910949', '874'),('LVSHFFAU0KS911445', '874'),('LVSHFFAU4KS911450', '874'),
-                   ('LVSHJCAL5KE910102', '87K'),('LVSHFFAU0KS910960', '874'),('LVSHFFAU2KS911429', '874'),('LVSHFFAU8KS911421', '874'),
-                   ('LVSHFFAUXKS911453', '874'),('LVSHFFAU6KS910848', '874'),('LVSHJCAL3KE908879', '87K'),('LVSHFFAU6KS911451', '874'),
-                   ('LVSHFFAU5KS908220', '874'),('LVSHFFAU4KS911741', '874'),('LVSHFFAU5KS911439', '874'),('LVSHFFAU4KS911447', '874'),
-                   ('LVSHFFAU8KS911449', '874'),('LVSHFFAUXKS911758', '874'),('LVSHFFAU8KS911452', '874'),('LVSHFFAU0KS911414', '874'),
-                   ('LVSHJCAL2KE909988', '87K'),('LVSHJCAL3KE910048', '87K'),('LVSHJCAL1KE910047', '87K')]
-tml = {'brand_model_name': 'CQ2VDC1', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
-                 'vin': 'XX0001', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name}
-
-for item in source_date:
-    tml['vin'] = item[0]
-    tml['product_model'] = item[1]
-    data_lst.append(tml)
-pprint.pprint(data_lst)
-# # 完成任务
-# # 227, 236, 254, 245
-# # 测试 I_WMS_AOP_001
-# data = {
-#     'params': {
-#         'data': [
-#             # {
-#             #     'task_id': 11,
-#             # },
-#             # {
-#             #     'task_id': 20,
-#             # },
-#             # {
-#             #     'task_id': 29,
-#             # },
-#             {
-#                 'task_id': 30,
-#                 # 'batch_id': 2
-#             }
-#         ]
-#     }
-# }
 
 res = requests.post(url=BASE_URL + DONE_PICKING, data=json.dumps(data), headers=HEADERS)
 content = json.loads(res.content.decode('utf-8'))
 print(content)
-# # 返回值： {'jsonrpc': '2.0', 'result': '{"time": 1571821697.9542348, "method": "/api/stock_picking/done_picking", "code": 200}', 'id': None}
 
-
-# 测试WMS
-# 使用 zeep 发送数据
+# data = {
+#     'params': {
+#         'data': data1
+#     }
+# }
+#
+# # data = {
+# #     'params': {
+# #         'data': []
+# #     }
+# # }
+# #
+# # data_lst = data['params']['data']
+# #
+# # source_date = [('LVSHJCAL5KE911041', '87K'),('LVSHFFAU2KS911432', '874'),('LVSHFFAU2KS911477', '874'),('LVSHFFAU3KS911438', '874'),
+# #                    ('LVSHFFAU5KS911702', '874'),('LVSHFFAU3KS911441', '874'),('LVSHFFAU7KS910681', '874'),('LVSHFFAU0KS910893', '874'),
+# #                    ('LVSHJCAL6KE908925', '87K'),('LVSHFFAUXKS911744', '874'),('LVSHFFAU8KS911435', '874'),('LVSHFFAU9KS911752', '874'),
+# #                    ('LVSHFFAUXKS911436', '874'),('LVSHFFAU5KS911750', '874'),('LVSHJCAL0KE910136', '87K'),('LVSHJCAL5KE910083', '87K'),
+# #                    ('LVSHFFAU1KS911745', '874'),('LVSHFFAUXKS910786', '874'),('LVSHJCAL8KE908926', '87K'),('LVSHFFAU7KS911457', '874'),
+# #                    ('LVSHFFAU3KS911455', '874'),('LVSHFFAU1KS910949', '874'),('LVSHFFAU0KS911445', '874'),('LVSHFFAU4KS911450', '874'),
+# #                    ('LVSHJCAL5KE910102', '87K'),('LVSHFFAU0KS910960', '874'),('LVSHFFAU2KS911429', '874'),('LVSHFFAU8KS911421', '874'),
+# #                    ('LVSHFFAUXKS911453', '874'),('LVSHFFAU6KS910848', '874'),('LVSHJCAL3KE908879', '87K'),('LVSHFFAU6KS911451', '874'),
+# #                    ('LVSHFFAU5KS908220', '874'),('LVSHFFAU4KS911741', '874'),('LVSHFFAU5KS911439', '874'),('LVSHFFAU4KS911447', '874'),
+# #                    ('LVSHFFAU8KS911449', '874'),('LVSHFFAUXKS911758', '874'),('LVSHFFAU8KS911452', '874'),('LVSHFFAU0KS911414', '874'),
+# #                    ('LVSHJCAL2KE909988', '87K'),('LVSHJCAL3KE910048', '87K'),('LVSHJCAL1KE910047', '87K')]
+# # tml = {'brand_model_name': 'CQ2VDC1', 'product_color': 'YG4F', 'product_model': '874', 'state_flag': 'T',
+# #                  'vin': 'XX0001', 'warehouse_code': warehouse_code, 'warehouse_name': warehouse_name}
+# #
+# # for item in source_date:
+# #     tml['vin'] = item[0]
+# #     tml['product_model'] = item[1]
+# #     data_lst.append(tml)
+# # pprint.pprint(data_lst)
+# # # 完成任务
+# # # 227, 236, 254, 245
+# task_ids = [1487, 1486, 1485, 1484, 1492, 1491, 1490, 1489, 1488]
+# data = []
+# for task_id in task_ids:
+#     tmp = {
+#         'task_id': task_id
+#     }
+#     data.append(tmp)
+# # 测试 I_WMS_AOP_001
+# data = {
+#     'params': {
+#         'data': data
+#     }
+# }
+#
+# res = requests.post(url=BASE_URL + DONE_PICKING, data=json.dumps(data), headers=HEADERS)
+# content = json.loads(res.content.decode('utf-8'))
+# print(content)
+# # # 返回值： {'jsonrpc': '2.0', 'result': '{"time": 1571821697.9542348, "method": "/api/stock_picking/done_picking", "code": 200}', 'id': None}
+#
+#
+# # 测试WMS
+# # 使用
+# # zeep
+# # 发送数据
 # from requests import Session
 # from zeep.transports import Transport
 # from zeep import Client
@@ -231,21 +260,12 @@ print(content)
 #
 #
 # # 任务
-# data = [{'task_id': 71, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'EM',
-#          'supplier_name': '重庆上源货运代理有限公司', 'warehouse_code': 'WTJCXBK', 'quantity_done': 1, 'brand_model_name': '长安福特',
-#          'from_location_id': '团结村线边库', 'to_location_id': '团结村站', 'to_location_type': 'internal', 'partner_name': '长安民生',
-#          'vin': 'LVSHFFAU3KS911441', 'picking_type_name': '装车', 'batch_id': 1,
-#          'scheduled_date': '2019-11-27 07:15:42'},
-#         {'task_id': 80, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'EM',
-#          'supplier_name': '重庆上源货运代理有限公司', 'warehouse_code': 'WTJCXBK', 'quantity_done': 1, 'brand_model_name': '长安福特',
-#          'from_location_id': '团结村线边库', 'to_location_id': '团结村站', 'to_location_type': 'internal', 'partner_name': '长安民生',
-#          'vin': 'LVSHFFAU7KS910681', 'picking_type_name': '装车', 'batch_id': 1,
-#          'scheduled_date': '2019-11-27 07:15:42'},
-#         {'task_id': 89, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'EM',
-#          'supplier_name': '重庆上源货运代理有限公司', 'warehouse_code': 'WTJCXBK', 'quantity_done': 1, 'brand_model_name': '长安福特',
-#          'from_location_id': '团结村线边库', 'to_location_id': '团结村站', 'to_location_type': 'internal', 'partner_name': '长安民生',
-#          'vin': 'LVSHFFAU0KS910893', 'picking_type_name': '装车', 'batch_id': 1,
-#          'scheduled_date': '2019-11-27 07:15:42'}]
+# data = {'picking_ids': [
+#     {'task_id': 9, 'product_name': '福睿斯', 'product_color': '1', 'product_model': '874', 'product_config': 'MK',
+#      'supplier_name': '供应商', 'warehouse_code': 'SXZZ', 'quantity_done': 1, 'brand_model_name': '长安福特',
+#      'from_location_id': '新筑站', 'to_location_id': '新筑站', 'to_location_type': 'internal', 'partner_name': '长安民生',
+#      'vin': 'QAZ12345234900006', 'picking_type_name': '卸车', 'batch_id': 1, 'scheduled_date': '2019-11-29 14:55:42',
+#      'sequence_id': 1}]}
 #
 # res = zeep_picking_client.service.sendToTask(str(data))
 # print(res)
