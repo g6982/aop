f8b31ce64bb1f387e84be660c7b4f298bfe3a3ce
1di0t <dgqcjx@gmail.com>
2019-12-06 13:55:19 +0800

    验证location

--- aop_sale/wizard/change_to_location_by_order_line.py
+++ aop_sale/wizard/change_to_location_by_order_line.py
@@ -27,7 +27,18 @@ class ChangeToLocationByOrderLine(models.TransientModel):
     # 先判断，选择的起点和终点，是否都在路由上
     # 再调度
     def dispatch_location_id_2_new_location_id(self):
-        pass
+        if not self.line_ids:
+            raise ValueError('You must select from/to location')
+        start_location_id = self.line_ids[0].from_location_id
+        end_location_id = self.line_ids[-1].to_location_id
+
+        for sale_order_line_id in self.order_line_ids:
+            from_location_ids = sale_order_line_id.route_id.rule_ids.mapped('location_src_id')
+            to_location_ids = sale_order_line_id.route_id.rule_ids.mapped('location_id')
+
+            # 验证位置在路由内
+            if start_location_id.id not in from_location_ids.ids or end_location_id.id not in to_location_ids.ids:
+                raise ValueError('Value Error')
 
 
 class ChangeToLocationByOrderLineDetail(models.TransientModel):
