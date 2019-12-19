1c7e886961039fceae97f28f79d11b94b43eb620
1di0t <dgqcjx@gmail.com>
2019-12-19 09:45:07 +0800

    fix

--- aop_sale/models/res_users_type.py
+++ aop_sale/models/res_users_type.py
@@ -8,5 +8,6 @@ _logger = logging.getLogger(__name__)
 
 class ResUsersType(models.Model):
     _name = 'res.users.type'
+    _sql_constraints = [('unique_name', 'unique(name)', 'The name must be unique')]
 
     name = fields.Char('Name')
