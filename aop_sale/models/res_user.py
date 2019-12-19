a31065e4e8852eb1f5cb073835e4c305c67222ea
1di0t <dgqcjx@gmail.com>
2019-12-19 09:58:41 +0800

    model first

--- aop_sale/models/res_user.py
+++ aop_sale/models/res_user.py
@@ -7,4 +7,4 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     allow_base_warehouse_ids = fields.Many2many('base.warehouse', string='Base')
-    # user_type_id = fields.Many2one('res.users.type', 'Type')
+    user_type_id = fields.Many2one('res.users.type', 'Type')
