5d31a968f1968ae5618c76f49b9c09cfac2658bb
waynexiao567890 <13671898587@163.com>
2020-01-08 17:13:27 +0800

    两端订单任务添加批量操作

--- aop_sale/wizard/disbatch_picking_to_sale_order.py
+++ aop_sale/wizard/disbatch_picking_to_sale_order.py
@@ -32,18 +32,18 @@ class DispatchPickingToSaleOrder(models.TransientModel):
             'company_id': self.company_id.id
         }
 
-    def judge_picking_correct(self):
-        if len(self.picking_ids) == 1:
+    def judge_picking_correct(self, picking_ids):
+        if len(picking_ids) == 1:
             return True
 
-        picking_ids = self.picking_ids.sorted(lambda x: x.id).ids
+        picking_ids = picking_ids.sorted(lambda x: x.id).ids
 
         if not sorted(picking_ids) == list(range(min(picking_ids), max(picking_ids) + 1)):
             raise ValueError('Error!')
 
         # 订单数据
-    def parse_from_to_location(self):
-        picking_ids = self.picking_ids.sorted(lambda x: x.id)
+    def parse_from_to_location(self, picking_ids):
+        picking_ids = picking_ids.sorted(lambda x: x.id)
 
         from_location_id = picking_ids[0].location_id
         to_location_id = picking_ids[-1].location_dest_id
@@ -58,20 +58,30 @@ class DispatchPickingToSaleOrder(models.TransientModel):
 
     # 订单行
     def parse_order_line_data(self):
-        self.judge_picking_correct()
 
         line_data = []
-        from_location_id, to_location_id = self.parse_from_to_location()
-        product_id = self.picking_ids[0].move_lines[0].product_id
-        tmp = {
-            'product_id': product_id.id,
-            'product_uom': product_id.uom_id.id,
-            'from_location_id': from_location_id.id if from_location_id else False,
-            'to_location_id': to_location_id.id if to_location_id else False
-        }
-        line_data.append(
-            (0, 0, tmp)
-        )
+
+        sale_order_line_ids = []
+        for sale_order_line_id, lines in groupby(self.picking_ids, lambda x: x.sale_order_line_id):
+            sale_order_line_ids.append(sale_order_line_id)
+
+        sale_order_line_ids = list(set(sale_order_line_ids))
+        for sale_order_line_id in sale_order_line_ids:
+            picking_ids = self.picking_ids.filtered(lambda x: x.sale_order_line_id.id == sale_order_line_id.id)
+            self.judge_picking_correct(picking_ids)
+
+            from_location_id, to_location_id = self.parse_from_to_location(picking_ids)
+            product_id = self.picking_ids[0].move_lines[0].product_id
+            tmp = {
+                'product_id': product_id.id,
+                'product_uom': product_id.uom_id.id,
+                'from_location_id': from_location_id.id if from_location_id else False,
+                'to_location_id': to_location_id.id if to_location_id else False
+            }
+            line_data.append(
+                (0, 0, tmp)
+            )
+
         return line_data
 
     # 创建销售订单
